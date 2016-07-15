bec45959ab746610d067fbbc9451cde16b1b1af1
Hannah Masila <hannahmasila@gmail.com>
2016-07-15 20:37:17 +0300

    initial commit with the structure of the application and the dependacies that will be needed. It also initialises the database

---
+++ server.js
@@ -0,0 +1,32 @@
+﻿require('rootpath')();
+var express = require('express');
+var app = express();
+var session = require('express-session');
+var bodyParser = require('body-parser'); // pull information from HTML POST
+var expressJwt = require('express-jwt');
+var config = require('config.json');
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views'); // set the static files location /public/img will be /img for us
+app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(session({ secret: config.secret, resave: false, saveUninitialized: true }));
+
+// use JWT auth to secure the api
+app.use('/api', expressJwt({ secret: config.secret }).unless({ path: ['/api/users/authenticate', '/api/users/register'] }));
+
+// routes
+app.use('/login', require('./controllers/login.controller'));
+app.use('/register', require('./controllers/register.controller'));
+app.use('/app', require('./controllers/app.controller'));
+app.use('/api/users', require('./controllers/api/users.controller'));
+
+// make '/app' default route
+app.get('/', function (req, res) {
+    return res.redirect('/app');
+});
+
+// start server
+var server = app.listen(3000, function () {
+    console.log('Server listening at http://' + server.address().address + ':' + server.address().port);
+});
